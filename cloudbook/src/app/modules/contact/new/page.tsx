b923f54ba3714199f1ee7dcbe20a0765aa746c2b
Sukumar-1706 <sukumarmee@gmail.com>
2025-07-10 11:41:32 +0530

    product correction fixed

--- cloudbook/src/app/modules/contact/new/page.tsx
+++ cloudbook/src/app/modules/contact/new/page.tsx
@@ -703,6 +703,7 @@ export default function NewContact() {
                     label="Active"
                     onChange={handleStatusChange}
                     checked={formData.otherDetails.status}
+                    className="mt-2"
                   />
                 </FormField>
               </div>
@@ -864,7 +865,7 @@ export default function NewContact() {
           <div className="flex-1 overflow-y-auto h-[calc(100vh-103px)]">
             <form ref={formRef} onSubmit={handleSubmit} autoComplete="off">
               <div className="border-b border-gray-300">
-                <div className="grid grid-cols-1 lg:grid-cols-2 px-4 py-2">
+                <div className="grid grid-cols-1 lg:grid-cols-2 px-4 py-2 mb-1">
                   <FormField label="Ledger Name" required htmlFor="ledgerName" className="!mb-0">
                     <div className="relative">
                       <div className="flex gap-2">
@@ -925,6 +926,7 @@ export default function NewContact() {
                         { value: "customer", label: "Sundry creditors" },
                         { value: "supplier", label: "Sundry debtors" },
                       ]}
+                      className="mt-2"
                       onChange={(e) =>
                         setFormData((pre) => ({
                           ...pre,
